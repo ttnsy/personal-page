b3f8610ad62dfcf47d0c3640b6059406e02da090
Tanesya <tanesya.t@gmail.com>
2024-01-22 17:14:43 +0700

    feat: adding :tada: :tada: confetti :tada: :tada:

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,30 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+// confetti
+import confetti from "canvas-confetti";
+const myCanvas = document.createElement("canvas");
+
+myCanvas.style.width = "100vw";
+myCanvas.style.height = "100vh";
+myCanvas.style.position = "fixed";
+myCanvas.style.inset = 0;
+myCanvas.style.right = 0;
+myCanvas.style.zIndex = -10;
+
+document.body.append(myCanvas);
+
+const confettiTextEl = document.querySelector(".text_confetti");
+
+confettiTextEl.addEventListener("click", () => {
+  const myConfetti = confetti.create(myCanvas, {
+    resize: true,
+    useWorker: true,
+  });
+
+  myConfetti({
+    particleCount: 200,
+    spread: 200,
+  });
+});
